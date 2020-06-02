6de85f3010da5c4b6d177405c339cd41e59d6275
diipacheco <edilsonpacheco6@gmail.com>
2020-06-02 02:06:42 -0300

    add sagas integration test

--- src/store/modules/countries/sagas.ts
+++ src/store/modules/countries/sagas.ts
@@ -1,3 +1,14 @@
+import { call, put } from 'redux-saga/effects';
+
+import api from '../../../services/api';
+import * as actions from './actions';
+
 export default function* gen() {
+  try {
+    const response = yield call(api.get, '/all');
+    yield put(actions.handleLoadSuccess(response.data));
+  } catch (error) {
+    yield put(actions.handleLoadFailure());
+  }
   return yield null;
 }
